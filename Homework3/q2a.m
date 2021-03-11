b59061138441c0a06ed773087b35bde887f0670a
Zavier-Ong <zacohjayjay@gmail.com>
2021-03-11 21:51:14 +0800

    q2a done

---
+++ Homework3/q2a.m
@@ -0,0 +1,72 @@
+clc
+clear
+close all
+
+% Matric A0138993L
+% Classes chosen: 9 and 3
+load('characters10.mat');
+
+%imshow(reshape(train_data(2997,:), [28,28]));
+train_idx = find(train_label == 3 | train_label == 9);
+% 9 --> 1 and 3 --> 0
+TrLabel = train_label(train_idx);
+TrLabel(TrLabel == 9) = 1;
+TrLabel(TrLabel == 3) = 0;
+
+train_x = train_data(train_idx, :);
+% casting to double to remove warning when normalizing data
+train_x = cast(train_x/255, 'double');
+
+test_idx = find(test_label == 3 | test_label == 9);
+TeLabel = test_label(test_idx);
+TeLabel(TeLabel == 9) = 1;
+TeLabel(TeLabel == 3) = 0;
+
+test_x = test_data(test_idx, :);
+% casting to double to remove warning when normalizing data
+test_x = cast(test_x/255, 'double');
+
+r_factors = [0, 0.0001, 0.001, 0.01, 0.1, 1, 10, 100];
+sd= 100;
+for i = r_factors
+    % exact interpolation on train
+    lambda = i;
+    r_train = pdist2(train_x, train_x, 'squaredeuclidean');
+    % gaussian rbf
+    phi = exp( r_train / (-2*((sd)^2)) );
+    % applying regularization method to determine new weights
+    if (i == 0)
+        w = phi \ TrLabel;
+    else
+        w = ((phi'*phi) + lambda*eye(size(phi, 2))) \ (phi'*TrLabel);
+    end
+    
+    % TrPred
+    TrPred = (phi*w)';
+    
+    %TePred
+    r_test = pdist2(test_x, train_x, 'squaredeuclidean');
+    %gaussian rbf
+    phi = exp( r_test / (-2*((sd)^2)) );
+    TePred = (phi*w)';
+
+    fig = figure();
+    TrAcc = zeros(1,1000);
+    TeAcc = zeros(1,1000);
+    thr = zeros(1,1000);
+    TrN = length(TrLabel);
+    TeN = length(TeLabel);
+    for j = 1:1000
+        t = (max(TrPred)-min(TrPred)) * (j-1)/1000 + min(TrPred);
+        thr(j) = t;
+        TrAcc(j) = (sum(TrLabel(TrPred<t)==0) + sum(TrLabel(TrPred>=t)==1)) / TrN;
+        TeAcc(j) = (sum(TeLabel(TePred<t)==0) + sum(TeLabel(TePred>=t)==1)) / TeN;
+    end
+    hold on
+    plot(thr, TrAcc, 'r');
+    plot(thr, TeAcc, '-xb');
+    legend('Train','Test');
+    title(sprintf('Exact Interpolation (lambda=%.4f)', i))
+    saveas(fig,sprintf('q2a_lambda_%.4f.png',i))
+    hold off
+end
